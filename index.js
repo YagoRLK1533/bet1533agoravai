5b32906c6162b9d735c9bdc7260f1d994144750a
YagoRLK1533 <yagorian92@icloud.com>
2025-04-20 21:51:07 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>BET1533</h1>
+      <p>Bem-vindo ao cassino online com Pix e jogos!</p>
+      <button style={{ padding: '10px 20px', fontSize: '18px' }}>
+        Deposite qualquer valor via Pix e jogue agora
+      </button>
+    </div>
+  )
+}
